91205e9fe382db7b0095d6e954ffa4fd8709e2c5
Anna-Kyra <anna-kyra.strik@hva.nl>
2024-10-13 14:57:06 +0200

    Begin header

--- src/lib/components/organisms/Header.svelte
+++ src/lib/components/organisms/Header.svelte
@@ -5,14 +5,63 @@
         </a>
     </h1>
     <nav>
-
+        <button aria-hidden="true">
+            menu
+        </button>
+        <ul>
+            <li><a href="/">Projecten</a></li>
+            <li><a href="/">I love web</a></li>
+            <li><a href="/">Over mij</a></li>
+            <li><a href="/">Contact</a></li>
+        </ul>
     </nav>
 </header>
 
 <style>
+    /******************************************************/
+    /* MOBILE FIRST */
+    /******************************************************/
     header {
         width: 100%;
         height: 100%;
+
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        gap: 2em;
+
+        padding: 1em 3em;
+
         background-color: teal;
+    }
+
+    header nav {
+        display: flex;
+        gap: 1em;
+    }
+
+    header nav ul{
+        display: flex;
+        gap: 1em;
+
+        list-style: none;
+    }
+
+    header nav ul a {
+        text-decoration: none;
+        text-transform: uppercase;
+    }
+
+    /******************************************************/
+    /* TABLET */
+    /******************************************************/
+
+    @media (min-width:38em) {
+    }
+
+    /******************************************************/
+    /* DESKTOP */
+    /******************************************************/
+    @media (min-width:54em) {
     }
 </style>
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,3 @@
-// place files you want to import through the `$lib` alias in this folder.
+// Organisms
 export { default as Footer } from '$lib/components/organisms/Footer.svelte'
 export { default as Header } from '$lib/components/organisms/Header.svelte'
