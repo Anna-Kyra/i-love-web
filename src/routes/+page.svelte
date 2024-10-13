515019ce61537660d5331fa3d6da830a9448aa60
Anna-Kyra <anna-kyra.strik@hva.nl>
2024-10-13 13:04:17 +0200

    sveltekit install 2
    - +page.js
    - global css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script>
+    /** @type {import('./$types').PageData} */
+    
+    console.log("Received data in +page.svelte:", data);
+	export let data
+    let people = data.people
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
