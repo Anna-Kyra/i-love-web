7852a746439444f60a7ade3e5452f854fcbeaae9
Anna-Kyra <anna-kyra.strik@hva.nl>
2024-11-02 14:47:21 +0100

    something

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,7 @@
-// Organisms
+// ORGANISMS
 export { default as Footer } from '$lib/components/organisms/Footer.svelte'
 export { default as Header } from '$lib/components/organisms/Header.svelte'
 export { default as Filter } from '$lib/components/organisms/Filter.svelte'
+export { default as BlogCard } from '$lib/components/organisms/Blog-card.svelte'
+
+// MOLECULES
